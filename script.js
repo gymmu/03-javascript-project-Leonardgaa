c81208695d8051adbcb73f31a434733a50258f82
e282250 <e282250@edu.sbl.ch>
2023-11-03 09:06:44 +0100

    Aufgabe 3

--- script.js
+++ script.js
@@ -33,12 +33,43 @@ export function aufgabe01(args) {
   export function  aufgabe03(args) {
     const input = args
     const result = []
+
+    let CountE = 0
+
     for (let i = 0; i < input.length; i++) {
       const currentElement = input[i]
-      if (currentElement === )
+
+      if (currentElement === "e") {
+        // zähle dieses e
+        CountE = CountE + 1
+      } else if (currentElement === "E") {
+        //zähle auch dieses Element
+        CountE = CountE +1 
+      }
+      
     }
-    return result.join("")
+    return CountE
   }
+
+  export function  aufgabe04 (args) {
+    const input = args
+    const result = []
+    
+    let CountCurrentElement = 1 
+
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      if (currentElement === " ") {
+        CountCurrentElement = CountCurrentElement + 1
+      }
+      else {
+        result.push(currentElement)
+      }
+    }
+    return CountCurrentElement
+  }
+
+
   
 
 
