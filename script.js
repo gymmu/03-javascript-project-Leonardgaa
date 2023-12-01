e00b2e59415295e573da54beb4f40dd88985baa1
e282250 <e282250@edu.sbl.ch>
2023-12-01 08:25:16 +0100

    Aufgabe 10 korrektur

--- script.js
+++ script.js
@@ -138,25 +138,25 @@ export function aufgabe06(args) {
     }
    
 export function aufgabe10 (args) {
-  const input = args
+   const input = args
   
-  if (input.length !== 7) return false
-  if (input[0] !== "#") return false
-  for (let i = 1; i < input.length; i++) {
-    const currentElement = input[i]
-    const ascii = currentElement.charCodeAT(0)
-
-    if ( 48 <= ascii && ascii <= 57) {
-
-    } else if ( 65 <= ascii && ascii <= 70){
-
-    }
-      else {
+   if(input.length !== 7) return false
+   if(input[0] !== "#") return false
+   for (let i = 1; i < input.length; i++) {
+     const currentElement = input[i]
+     const ascii = currentElement.charCodeAt(0)
+   
+    if(48 <= ascii && ascii <=57) {
+         
+     } else if (65 <= ascii && ascii <= 70) {
+          
+     } else {
        return false
-    }
-  }
-   return true
-}
+     }
+   }
+    
+    return true
+  }
   
 export function  aufgabe11 (args) {
   const input = args
